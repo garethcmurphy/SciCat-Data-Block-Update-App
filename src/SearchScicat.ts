7f71983a001e575f25f8d00f022b9b2469b0db41
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-06 16:02:47 +0200

    add extra classes

---
+++ src/SearchScicat.ts
@@ -0,0 +1,14 @@
+class SearchScicat {
+    
+    search() {
+
+    }
+
+
+}
+
+
+if (require.main === module) {
+    const fix = new SearchScicat();
+    fix.search();
+  }
--- src/datablocks.ts
+++ src/datablocks.ts
@@ -1,7 +1,13 @@
 class FixBlocks {
+  api_url = "https://scicatapi.esss.dk/api/v3";
+
+
+
   fix() {
+
     console.log("fix");
   }
+
 
 
 }
