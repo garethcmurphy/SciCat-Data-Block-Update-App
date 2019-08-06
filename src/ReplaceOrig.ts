63909b0bc69938edc77a4b7e04dbc356b17ace5a
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-06 16:34:07 +0200

    add new replace orig

--- src/ReplaceOrig.ts
+++ src/ReplaceOrig.ts
@@ -1,16 +1,28 @@
 import * as request from "request-promise";
+import { FilesInfo } from "./filesinfo";
 
 class ReplaceOrig {
-    base_url = "https://scicatapi.esss.dk/api/v3";
+  base_url = "https://scicatapi.esss.dk/api/v3";
 
-    postToScicat() {
-        const uri = this.base_url;
-        request.get(uri);
+  postToScicat() {
+    const uri = this.base_url;
+    request.get(uri);
+    // delete old orig for pid
+    // fetch file info
+    const fileInfo = new FilesInfo("demo/nexus_00000490.hdf");
+    const info = fileInfo.files;
+    console.log(info);
+    // add new orig
+  }
 
-    }
+  delete_old_orig(pid: string) {
+    console.log("deleting", pid);
+  }
 
+  add_new_orig(pid: string) {
+    console.log("adding new orig", pid);
+  }
 }
-
 
 if (require.main === module) {
   const fix = new ReplaceOrig();
