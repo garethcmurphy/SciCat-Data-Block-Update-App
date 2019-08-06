56e98ecaac1e709d6efdbe7b22aac574d2d7c2b5
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-06 15:53:45 +0200

    fix form,at

--- datablocks.ts
+++ datablocks.ts
@@ -1,14 +1,10 @@
 class FixBlocks {
-
-    fix() {
-        console.log("fix");
-    }
-
+  fix() {
+    console.log("fix");
+  }
 }
 
-
 if (require.main === module) {
-    // this module was run directly from the command line as in node xxx.js
-    const fix = new FixBlocks()
-    fix.fix()
+  const fix = new FixBlocks();
+  fix.fix();
 }
