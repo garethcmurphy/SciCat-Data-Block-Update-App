5bbbdc37e7e5c9868bbe14e6fb6a87671eff6a9f
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-18 22:27:46 +0200

    add updates

--- src/SearchScicat.ts
+++ src/SearchScicat.ts
@@ -3,21 +3,25 @@ import * as request from "request-promise";
 export class SearchScicat {
   api_url = "https://scicatapi.esss.dk/api/v3/";
 
-    encoder(filter: Object) {
-      const string = JSON.stringify(filter);
-      const encode = encodeURIComponent(string);
-        return encode;
-    }
+  encoder(filter: Object) {
+    const string = JSON.stringify(filter);
+    const encode = encodeURIComponent(string);
+    return encode;
+  }
 
   async search(searchTerm: string) {
     const filter = { text: searchTerm };
     const limits = { limit: 1, order: "creationTime asc" };
 
-      const encodeFilter = this.encoder(filter);
-      const encodeLimit = this.encoder(limits);
+    const encodeFilter = this.encoder(filter);
+    const encodeLimit = this.encoder(limits);
 
-      const uri = this.api_url + "Datasets/anonymousquery?fields=" +
-          encodeFilter + "&limits=" + encodeLimit;
+    const uri =
+      this.api_url +
+      "Datasets/anonymousquery?fields=" +
+      encodeFilter +
+      "&limits=" +
+      encodeLimit;
     const response = await request.get(uri);
     const array = JSON.parse(response);
     return array;
--- src/datablocks.ts
+++ src/datablocks.ts
@@ -1,14 +1,7 @@
 class FixBlocks {
-
-
-
   fix() {
-
     console.log("fix");
   }
-
-
-
 }
 
 if (require.main === module) {
